1a51ab864e73951dd92e5816b1a0ddb137dc415f
NGUYEN DANG HOANG <nguyendanghoang@Mac-mini-cua-admin-4.local>
2025-05-09 17:31:50 +0700

    Initial commit

--- app/src/main/java/com/example/appquanly/ThucDon/view/ThucDonActivity.kt
+++ app/src/main/java/com/example/appquanly/ThucDon/view/ThucDonActivity.kt
@@ -12,7 +12,7 @@ class ThucDonActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_thuc_don)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainthucdon)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
